35f4a752c2288538fbac3727c3fa1e1e423945c6
NovaPrime18 <novaprime18@gmail.com>
2021-07-01 13:58:39 +0200

    Added SchroombortFirmware folder

---
+++ HardwareSchroombortFirmware/HardwareSchroombortFirmware/main.c
@@ -0,0 +1,17 @@
+/*
+ * HardwareSchroombortFirmware.c
+ *
+ * Created: 7/1/2021 1:50:30 PM
+ * Author : thomm
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while (1)
+    {
+		printf("schroom heel hard");
+    }
+}
+
